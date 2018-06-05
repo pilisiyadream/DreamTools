1cfbc3eb8571db202a9de8db8f84979b9c03c489
xujiang <1136051009@qq.com>
2018-06-05 23:28:25 +0800

    Initial commit

---
+++ dreamtools/src/main/java/com/pilisiya/dreamtools/util/StringUtil.java
@@ -0,0 +1,42 @@
+package com.pilisiya.dreamtools.util;
+
+/**
+ * Created by xujiang on 2018/6/5.
+ */
+
+public class StringUtil {
+    /**
+     * 字符串转16进制字符串
+     */
+    public static String string2HexString(String s) throws Exception {
+        String r = bytes2HexString(string2Bytes(s));
+        return r;
+    }
+
+    /**
+     * 字节数组转16进制字符串
+     */
+    public static String bytes2HexString(byte[] b) {
+        String r = "";
+
+        for (int i = 0; i < b.length; i++) {
+            String hex = Integer.toHexString(b[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            r += hex.toUpperCase();
+        }
+
+        return r;
+    }
+
+    /**
+     * 字符串转字节数组
+     */
+    public static byte[] string2Bytes(String s) {
+        byte[] r = s.getBytes();
+        return r;
+    }
+
+
+}
