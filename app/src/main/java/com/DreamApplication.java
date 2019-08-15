1fd42ac57b64d01f34b1565d54a32b58ad4a2bc5
xujiang <1136051009@qq.com>
2019-08-15 16:14:14 +0800

    ----新增百度语音播报----

---
+++ app/src/main/java/com/DreamApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import android.app.Application;
+
+/**
+ * 说明： Copyright 2019 福建联迪商用设备有限公司. All rights reserved.
+ * 备注：
+ *
+ * @author created by xujiang on 2019/8/12
+ * 包名：com
+ * 电话: 17752885689
+ */
+public class DreamApplication extends Application {
+}
---
+++ dreamtools/src/main/java/com/pilisiya/dreamtools/tts/TTsConstant.java
@@ -0,0 +1,12 @@
+package com.pilisiya.dreamtools.tts;
+
+/**
+ * 说明： Copyright 2019 福建联迪商用设备有限公司. All rights reserved.
+ * 备注：
+ *
+ * @author created by xujiang on 2019/8/12
+ * 包名：com.pilisiya.dreamtools.tts
+ * 电话: 17752885689
+ */
+public class TTsConstant {
+}
