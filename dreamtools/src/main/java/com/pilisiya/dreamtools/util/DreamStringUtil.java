5b5afd62b8fda0ad8b90b04e819100672fe54c15
xujiang <xujiang@landicorp.com>
2020-12-02 10:20:09 +0800

    修改16进制字符串转换

--- dreamtools/src/main/java/com/pilisiya/dreamtools/util/DreamStringUtil.java
+++ dreamtools/src/main/java/com/pilisiya/dreamtools/util/DreamStringUtil.java
@@ -5,6 +5,8 @@ package com.pilisiya.dreamtools.util;
  */
 
 public class DreamStringUtil {
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+
     /**
      * 字符串转16进制字符串
      */
@@ -35,6 +37,37 @@ public class DreamStringUtil {
     public static byte[] string2Bytes(String s) {
         byte[] r = s.getBytes();
         return r;
+    }
+
+    public static byte[] hexString2Bytes(String data) {
+        if (isNullEmpty(data)) {
+            return EMPTY_BYTE_ARRAY;
+        } else {
+            byte[] result = new byte[(data.length() + 1) / 2];
+            if ((data.length() & 1) == 1) {
+                data = data + "0";
+            }
+
+            for (int i = 0; i < result.length; ++i) {
+                result[i] = (byte) (hex2byte(data.charAt(i * 2 + 1)) | hex2byte(data.charAt(i * 2)) << 4);
+            }
+
+            return result;
+        }
+    }
+
+    public static byte hex2byte(char hex) {
+        if (hex <= 'f' && hex >= 'a') {
+            return (byte) (hex - 97 + 10);
+        } else if (hex <= 'F' && hex >= 'A') {
+            return (byte) (hex - 65 + 10);
+        } else {
+            return hex <= '9' && hex >= '0' ? (byte) (hex - 48) : 0;
+        }
+    }
+
+    public static boolean isNullEmpty(String str) {
+        return str == null || str.length() == 0;
     }
 
 
